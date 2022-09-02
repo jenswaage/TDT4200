6e939ddfcf191656c28b4ab9beac758773048bb5
Jens Waage <waage.jens@gmail.com>
2022-09-02 19:06:30 +0200

    Finish PS0

--- PS0/code/src/ps0.c
+++ PS0/code/src/ps0.c
@@ -27,9 +27,16 @@ int main() {
     float* derivative_vals = allocateArray(N);
 
     for(int i = 0; i < N; i = i + 1) {
+        if (i > 0 && i < (N - 1)) {
+            derivative_vals[i] = centralDifference(i);
+        }
         function_vals[i] = square(i);
     }
+    printf("Function values:\n");
     printValues(function_vals, N);
+    printf("\n");
+    printf("Derivative values:\n");
+    printValues(derivative_vals, N);
 
     return 1;
 }
