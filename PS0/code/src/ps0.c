c54d81989e2248091ed35c3f08a88bc918728885
Jens Waage <waage.jens@gmail.com>
2022-09-02 18:59:05 +0200

    Implement square function and fill array

--- PS0/code/src/ps0.c
+++ PS0/code/src/ps0.c
@@ -12,8 +12,20 @@ float square(float x) {
     return x * x;
 }
 
+void printValues(float* array, int size) {
+    for(int i = 0; i < size; i = i + 1) {
+        printf("%.2f\n", array[i]);
+    }
+}
+
 int main() {
     float* function_vals = allocateArray(N);
     float* derivative_vals = allocateArray(N);
+
+    for(int i = 0; i < N; i = i + 1) {
+        function_vals[i] = square(i);
+    }
+    printValues(function_vals, N);
+
     return 1;
 }
