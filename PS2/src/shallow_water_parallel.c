45d7ee8a051a990014cee32356506835db00d6f1
Jens Waage <waage.jens@gmail.com>
2022-09-27 19:13:19 +0200

    PS2: Finish implementation

--- PS2/src/shallow_water_parallel.c
+++ PS2/src/shallow_water_parallel.c
@@ -17,8 +17,8 @@ MPI_Comm
 MPI_Datatype
     grid,
     subgrid,
-    y_column,
-    x_column;
+    column, // extracts a column from the grid
+    row; // extracts a row from the grid
 
 int
     rank,
@@ -29,7 +29,11 @@ int
 // Cartesian information
 int n_dims = 2; // number of dimensions for cartesian communicator
 int periods[2] = {0, 0}; // periodic information for dimensions
-int *dims; // pointer to array of number of processes in each dimension
+int dims[2]; // pointer to array of number of processes in each dimension
+
+// cartesian neighbors
+int y_neighbor_up, y_neighbor_down,
+    x_neighbor_left, x_neighbor_right; 
 
 #define MPI_RANK_ROOT  ( rank == 0 )
 
@@ -92,6 +96,31 @@ swap ( real_t** t1, real_t** t2 )
 	*t1 = *t2;
 	*t2 = tmp;
 }
+/**
+ * Communicate values along grid borders.
+ * We expect to both send to and receive from our comm_target, but in different buffers.
+**/
+void 
+communicate(int send_y, int send_x, int recv_y, int recv_x, MPI_Datatype type, int comm_target) {
+    MPI_Sendrecv(&DU(send_y, send_x), 1, type, comm_target, 0, 
+                 &DU(recv_y, recv_x), 1, type, comm_target, 0, 
+                 cart, MPI_STATUS_IGNORE);
+    MPI_Sendrecv(&DV(send_y, send_x), 1, type, comm_target, 0, 
+                 &DV(recv_y, recv_x), 1, type, comm_target, 0, 
+                 cart, MPI_STATUS_IGNORE);
+    MPI_Sendrecv(&PN(send_y, send_x), 1, type, comm_target, 0, 
+                 &PN(recv_y, recv_x), 1, type, comm_target, 0, 
+                 cart, MPI_STATUS_IGNORE);
+    MPI_Sendrecv(&PNU(send_y, send_x), 1, type, comm_target, 0, 
+                 &PNU(recv_y, recv_x), 1, type, comm_target, 0, 
+                 cart, MPI_STATUS_IGNORE);             
+    MPI_Sendrecv(&PNV(send_y, send_x), 1, type, comm_target, 0, 
+                 &PNV(recv_y, recv_x), 1, type, comm_target, 0, 
+                 cart, MPI_STATUS_IGNORE);
+    MPI_Sendrecv(&PNUV(send_y, send_x), 1, type, comm_target, 0, 
+                 &PNUV(recv_y, recv_x), 1, type, comm_target, 0, 
+                 cart, MPI_STATUS_IGNORE);
+}
 
 
 int
@@ -104,9 +133,12 @@ main ( int argc, char **argv )
     MPI_Comm_rank ( MPI_COMM_WORLD, &rank );  
     
     // Allocate processes to grid
-    dims = calloc(n_dims, sizeof(int)); // allocate adequately sized array   
     MPI_Dims_create(comm_size, n_dims, dims); // find number of processes in each dimension
     MPI_Cart_create(MPI_COMM_WORLD, n_dims, dims, periods, 0, &cart); 
+
+    // Detect neighbors
+    MPI_Cart_shift(cart, 0, 1, &y_neighbor_up, &y_neighbor_down); // we shift 1 down
+    MPI_Cart_shift(cart, 1, 1, &x_neighbor_left, &x_neighbor_right); // we shift 1 right
 
     if ( MPI_RANK_ROOT )
     {
@@ -141,95 +173,40 @@ main ( int argc, char **argv )
     for ( int_t iteration = 0; iteration<=max_iteration; iteration++ )
     {
         // TODO 5 Implement border exchange
-        int 
-            y_neighbor_up, y_neighbor_down,
-            x_neighbor_left, x_neighbor_right;
-        
-        MPI_Cart_shift(cart, 0, 1, &y_neighbor_up, &y_neighbor_down);
-        MPI_Cart_shift(cart, 1, 1, &x_neighbor_left, &x_neighbor_right);
 
-        if (y_neighbor_down != MPI_PROC_NULL) { // We have a downstairs neighbor
-            MPI_Sendrecv(&DU(1, 1), 1, x_column, y_neighbor_down, 0, 
-                         &DU(0, 1), 1, x_column, y_neighbor_down, 0, 
-                         cart, MPI_STATUS_IGNORE);
-            MPI_Sendrecv(&DV(1, 1), 1, x_column, y_neighbor_down, 0, 
-                         &DU(0, 1), 1, x_column, y_neighbor_down, 0, 
-                         cart, MPI_STATUS_IGNORE);
-            MPI_Sendrecv(&PN(1, 1), 1, x_column, y_neighbor_down, 0, 
-                         &PN(0, 1), 1, x_column, y_neighbor_down, 0, 
-                         cart, MPI_STATUS_IGNORE);
-            MPI_Sendrecv(&PNU(1, 1), 1, x_column, y_neighbor_down, 0, 
-                         &PNU(0, 1), 1, x_column, y_neighbor_down, 0, 
-                         cart, MPI_STATUS_IGNORE);             
-            MPI_Sendrecv(&PNV(1, 1), 1, x_column, y_neighbor_down, 0, 
-                         &PNV(0, 1), 1, x_column, y_neighbor_down, 0, 
-                         cart, MPI_STATUS_IGNORE);
-            MPI_Sendrecv(&PNUV(1, 1), 1, x_column, y_neighbor_down, 0, 
-                         &PNUV(0, 1), 1, x_column, y_neighbor_down, 0, 
-                         cart, MPI_STATUS_IGNORE);
+        // We can use the neighbor information to determine which nodes we will send to and receive from
+
+        if (y_neighbor_up != MPI_PROC_NULL) { // We have a neighbor above
+            // Send values in row starting at (1,1) and ending at (1, local_rows)
+            //      - top row of subgrid 
+            // Receive values in row starting at (0,1) and ending at (0, local_rows)
+            //      - top padding of subgrid
+            communicate(1, 1, 0, 1, row, y_neighbor_up);
         } 
         
-        if (y_neighbor_up != MPI_PROC_NULL) { // we have an upstairs neighbor
-            MPI_Sendrecv(&DU(local_rows - 1, 1), 1, x_column, y_neighbor_up, 0, 
-                         &DU(local_rows + 1, 1), 1, x_column, y_neighbor_up, 0, 
-                         cart, MPI_STATUS_IGNORE);
-            MPI_Sendrecv(&DV(local_rows - 1, 1), 1, x_column, y_neighbor_up, 0, 
-                         &DV(local_rows + 1, 1), 1, x_column, y_neighbor_up, 0, 
-                         cart, MPI_STATUS_IGNORE);
-            MPI_Sendrecv(&PN(local_rows - 1, 1), 1, x_column, y_neighbor_up, 0, 
-                         &PN(local_rows + 1, 1), 1, x_column, y_neighbor_up, 0, 
-                         cart, MPI_STATUS_IGNORE);
-            MPI_Sendrecv(&PNU(local_rows - 1, 1), 1, x_column, y_neighbor_up, 0, 
-                         &PNU(local_rows + 1, 1), 1, x_column, y_neighbor_up, 0, 
-                         cart, MPI_STATUS_IGNORE);
-            MPI_Sendrecv(&PNV(local_rows - 1, 1), 1, x_column, y_neighbor_up, 0, 
-                         &PNV(local_rows + 1, 1), 1, x_column, y_neighbor_up, 0, 
-                         cart, MPI_STATUS_IGNORE);
-            MPI_Sendrecv(&PNUV(local_rows - 1, 1), 1, x_column, y_neighbor_up, 0, 
-                         &PNUV(local_rows + 1, 1), 1, x_column, y_neighbor_up, 0, 
-                         cart, MPI_STATUS_IGNORE);
+        if (y_neighbor_down != MPI_PROC_NULL) { // we have a neighbor below
+            // Send values in row starting at (local_rows, 1) and ending at (local_rows, local_cols)
+            //      - bottom row of subgrid 
+            // Receive values in row starting at (local_rows + 1,1) and ending at (local_rows + 1, local_cols)
+            //      - bottom padding of subgrid
+            communicate(local_rows, 1, local_rows + 1, 1, row, y_neighbor_down);
         }
 
-        if (x_neighbor_right != MPI_PROC_NULL) {
-            MPI_Sendrecv(&DU(1, local_cols), 1, y_column, x_neighbor_right, 0, 
-                         &DU(1, local_cols + 1), 1, y_column, x_neighbor_right, 0, 
-                         cart, MPI_STATUS_IGNORE);
-            MPI_Sendrecv(&DV(1, local_cols), 1, y_column, x_neighbor_right, 0, 
-                         &DV(1, local_cols + 1), 1, y_column, x_neighbor_right, 0, 
-                         cart, MPI_STATUS_IGNORE);
-            MPI_Sendrecv(&PN(1, local_cols), 1, y_column, x_neighbor_right, 0, 
-                         &PN(1, local_cols + 1), 1, y_column, x_neighbor_right, 0, 
-                         cart, MPI_STATUS_IGNORE);
-            MPI_Sendrecv(&PNU(1, local_cols), 1, y_column, x_neighbor_right, 0, 
-                         &PNU(1, local_cols + 1), 1, y_column, x_neighbor_right, 0, 
-                         cart, MPI_STATUS_IGNORE);
-            MPI_Sendrecv(&PNV(1, local_cols), 1, y_column, x_neighbor_right, 0, 
-                         &PNV(1, local_cols + 1), 1, y_column, x_neighbor_right, 0, 
-                         cart, MPI_STATUS_IGNORE);
-            MPI_Sendrecv(&PNUV(1, local_cols), 1, y_column, x_neighbor_right, 0, 
-                         &PNUV(1, local_cols + 1), 1, y_column, x_neighbor_right, 0, 
-                         cart, MPI_STATUS_IGNORE);
+        if (x_neighbor_right != MPI_PROC_NULL) { // we have a neighbor to the right
+            // Send values in column starting at (1, local_cols) and ending at (local_rows, local_cols)
+            //      - left column of subgrid 
+            // Receive values in row starting at (1,local_cols+1) and ending at (local_rows, local_cols+1)
+            //      - left padding of subgrid
+            communicate(1, local_cols, 1, local_cols + 1, column, x_neighbor_right);
+            
         } 
         
-        if (x_neighbor_left != MPI_PROC_NULL) {
-            MPI_Sendrecv(&DU(1, 1), 1, y_column, x_neighbor_left, 0, 
-                         &DU(1, 0), 1, y_column, x_neighbor_left, 0, 
-                         cart, MPI_STATUS_IGNORE);
-            MPI_Sendrecv(&DV(1, 1), 1, y_column, x_neighbor_left, 0, 
-                         &DV(1, 0), 1, y_column, x_neighbor_left, 0, 
-                         cart, MPI_STATUS_IGNORE);
-            MPI_Sendrecv(&PN(1, 1), 1, y_column, x_neighbor_left, 0, 
-                         &PN(1, 0), 1, y_column, x_neighbor_left, 0, 
-                         cart, MPI_STATUS_IGNORE);
-            MPI_Sendrecv(&PNU(1, 1), 1, y_column, x_neighbor_left, 0, 
-                         &PNU(1, 0), 1, y_column, x_neighbor_left, 0, 
-                         cart, MPI_STATUS_IGNORE);
-            MPI_Sendrecv(&PNV(1, 1), 1, y_column, x_neighbor_left, 0, 
-                         &PNV(1, 0), 1, y_column, x_neighbor_left, 0, 
-                         cart, MPI_STATUS_IGNORE);
-            MPI_Sendrecv(&PNUV(1, 1), 1, y_column, x_neighbor_left, 0, 
-                         &PNUV(1, 0), 1, y_column, x_neighbor_left, 0, 
-                         cart, MPI_STATUS_IGNORE);
+        if (x_neighbor_left != MPI_PROC_NULL) { // we have a neighbor to the left
+            // Send values in column starting at (1, 1) and ending at (local_rows, 1)
+            //      - right column of subgrid 
+            // Receive values in row starting at (1,0) and ending at (local_rows, 0)
+            //      - right padding of subgrid
+            communicate(1, 1, 1, 0, column, x_neighbor_left);
         }
 
         // TODO 4 Change application of boundary condition to match cartesian topology
@@ -279,6 +256,8 @@ time_step ( void )
 {
     // TODO 3 Update the area of iteration in the time step
 
+    // we only change the iteration dimension to iterate over the subgrid instead of entire grid
+
     for ( int_t y=1; y<=local_rows; y++ )
         for ( int_t x=1; x<=local_cols; x++ )
         {
@@ -292,7 +271,8 @@ time_step ( void )
             PNUV(y,x) = PN(y,x) * U(y,x) * V(y,x);
         }
 
-    for ( int_t y=0; y<=local_rows+1; y++ )
+    // velocity updates are for every square
+    for ( int_t y=0; y<=local_rows+1; y++ ) 
         for ( int_t x=0; x<=local_cols+1; x++ )
         {
             DU(y,x) = PN(y,x) * U(y,x) * U(y,x)
@@ -337,16 +317,28 @@ boundary_condition ( real_t *domain_variable, int sign )
 
     #define VAR(y,x) domain_variable[(y)*(local_cols+2)+(x)]
 
-    VAR(   0, 0   ) = sign*VAR(   2, 2   );
-    VAR( local_rows+1, 0   ) = sign*VAR( local_rows-1, 2   );
-    VAR(   0, local_cols+1 ) = sign*VAR(   2, local_cols-1 );
-    VAR( local_rows+1, local_cols+1 ) = sign*VAR( local_rows-1, local_cols-1 );
-
-    for ( int_t y=1; y<=local_rows; y++ ) VAR(   y, 0   ) = sign*VAR(   y, 2   );
-    for ( int_t y=1; y<=local_rows; y++ ) VAR(   y, local_cols+1 ) = sign*VAR(   y, local_cols-1 );
-    for ( int_t x=1; x<=local_cols; x++ ) VAR(   0, x   ) = sign*VAR(   2, x   );
-    for ( int_t x=1; x<=local_cols; x++ ) VAR( local_rows+1, x   ) = sign*VAR( local_rows-1, x   );
+    // We need to set boundary conditions when we know we don't have a neighbor in a direction
     
+    if (x_neighbor_left == MPI_PROC_NULL) { // we don't have a left neighbor
+        // we have to set left boundary
+        for ( int_t y=1; y<=local_rows; y++ ) VAR(   y, 0   ) = sign*VAR(   y, 2   ); // left boundary
+    }
+
+    if (x_neighbor_right == MPI_PROC_NULL) { // we don't have a right neighbor
+        // we have to set right boundary, including corner
+        for ( int_t y=1; y<=local_rows; y++ ) VAR(   y, local_cols+1 ) = sign*VAR(   y, local_cols-1 ); // right boundary            
+    }
+
+    if (y_neighbor_down == MPI_PROC_NULL) { // we don't have a bottom neighbor
+        // we have to set bottom boundary
+        for ( int_t x=1; x<=local_cols; x++ ) VAR( local_rows+1, x   ) = sign*VAR( local_rows-1, x   ); //bottom boundary  
+    }
+
+    if (y_neighbor_up == MPI_PROC_NULL) { // we don't have a top neighbor
+        // we have to set top boundary
+        for ( int_t x=1; x<=local_cols; x++ ) VAR(   0, x   ) = sign*VAR(   2, x   ); // top boundary
+    }
+
     #undef VAR
 }
 
@@ -365,11 +357,11 @@ create_types ( void )
     MPI_Type_commit ( &grid ) ;
 
     // create column type
-    MPI_Type_vector(local_rows, 1, local_cols + 2, MPI_DOUBLE, &y_column);
-    MPI_Type_vector(local_cols, 1, 1, MPI_DOUBLE, &x_column);
+    MPI_Type_vector(local_rows, 1, local_cols + 2, MPI_DOUBLE, &column);
+    MPI_Type_vector(local_cols, 1, 1, MPI_DOUBLE, &row);
 
-    MPI_Type_commit(&y_column);
-    MPI_Type_commit(&x_column);
+    MPI_Type_commit(&column);
+    MPI_Type_commit(&row);
 
 }
 
@@ -379,6 +371,7 @@ domain_init ( void )
 {
     // TODO 2 Find the number of columns and rows of each subgrid
     // Hint: you can get useful information from the cartesian communicator
+    
     local_rows  = N / dims[0]; // number of rows = discretization steps / number of processes in dim 0 (evenly divisible) 
     local_cols  = N / dims[1]; // number of cols = discretization steps / number of processes in dim 1 (evenly divisible)
 
@@ -402,6 +395,7 @@ domain_init ( void )
 
     // TODO 2 Find the local x and y offsets for each process' subgrid
     // Hint: you can get useful information from the cartesian communicator
+    
     int coords[n_dims]; // store process coordinates
 
     MPI_Cart_coords(cart, rank, n_dims, coords);
@@ -435,8 +429,6 @@ domain_init ( void )
 
     dx = domain_size / (real_t) N;
     dt = 5e-2;
-
-    free(dims); // no longer needed
 }
 
 
